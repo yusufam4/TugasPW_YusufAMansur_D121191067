051a4229b1fd457fb808fdd84a9113538d0e463d
yusufam4 <yusufgit4@gmail.com>
2021-06-06 12:08:09 +0800

    Menambahkan file

---
+++ Pertemuan_11/hapus.php
@@ -0,0 +1,21 @@
+<?php
+
+require 'functions.php';
+
+// jika tidak ada id di url
+if (!isset($_GET['id'])) {
+  header("location: index.php");
+  exit;
+}
+
+// mengambil id dari url
+$id = $_GET['id'];
+
+if (hapus($id) > 0) {
+  echo "<script>
+          alert('Data berhasil dihapus!');
+          document.location.href = 'index.php';
+        </script>";
+} else {
+  echo "Data gagal dihapus!";
+}
---
+++ Pertemuan_13/ajax/ajax_cari.php
@@ -0,0 +1,34 @@
+<?php
+require '../functions.php';
+$mahasiswa = cari($_GET['keyword']);
+?>
+
+<table border="1" cellpadding="10" cellspacing="0">
+  <tr>
+    <th>#</th>
+    <th>Gambar</th>
+    <th>Nama</th>
+    <th>Aksi</th>
+  </tr>
+
+  <?php if (empty($mahasiswa)) : ?>
+    <tr>
+      <td colspan="4">
+        <p>data mahasiswa tidak ditemukan!</p>
+      </td>
+    </tr>
+
+  <?php endif; ?>
+
+  <?php $i = 1;
+  foreach ($mahasiswa as $m) : ?>
+    <tr>
+      <td><?= $i++; ?></td>
+      <td><img src="<?= $m['gambar']; ?>" width="60"></td>
+      <td><?= $m['nama']; ?></td>
+      <td>
+        <a href="detail.php?id=<?= $m['id']; ?>">lihat detailnya</a>
+      </td>
+    </tr>
+  <?php endforeach; ?>
+</table>
