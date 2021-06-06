0628dd03afb9b9ab5d3a96d588b16103e2f59e08
yusufam4 <yusufgit4@gmail.com>
2021-06-06 14:54:33 +0800

    menambahkan file

--- Pertemuan_13/ajax/ajax_cari.php
+++ Pertemuan_13/ajax/ajax_cari.php
@@ -24,7 +24,7 @@ $mahasiswa = cari($_GET['keyword']);
   foreach ($mahasiswa as $m) : ?>
     <tr>
       <td><?= $i++; ?></td>
-      <td><img src="<?= $m['gambar']; ?>" width="60"></td>
+      <td><img src="img/<?= $m['gambar']; ?>" width="60"></td>
       <td><?= $m['nama']; ?></td>
       <td>
         <a href="detail.php?id=<?= $m['id']; ?>">lihat detailnya</a>
